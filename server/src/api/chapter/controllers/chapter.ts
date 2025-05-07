30dc3991a6ac1bbae828bf77ff522af9431c4b41
ayPromise servant <fatherpother@gmail.com>
2025-05-07 22:38:01 +0300

    <backend>: feat: updated Chapter Controller+Model+Routes policy set to auth

--- server/src/api/chapter/controllers/chapter.ts
+++ server/src/api/chapter/controllers/chapter.ts
@@ -4,4 +4,22 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::chapter.chapter');
+export default factories.createCoreController('api::chapter.chapter', ({strapi})=>({
+    async findOne(ctx){
+        const user = ctx.state.user
+        const chapterId = ctx.params.id
+        const chapter = await strapi.db.query('api::chapter.chapter').findOne({where:{id:chapterId},  
+        
+            populate: { book: { populate: ['owner'] } }})
+
+        if (!chapter) {
+            return ctx.notFound('Chapter does not exist');
+        }
+
+        if(chapter.book.owner.id !== user.id)
+            return ctx.unauthorized('You have no access to that book')
+
+        return ctx.send({ data: chapter });
+    }
+}));
+
--- server/src/api/chapter/routes/chapter.ts
+++ server/src/api/chapter/routes/chapter.ts
@@ -2,6 +2,22 @@
  * chapter router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi"
 
-export default factories.createCoreRouter('api::chapter.chapter');
+export default factories.createCoreRouter('api::chapter.chapter', {
+  config:{
+    find:{
+      policies:['global::is-authenticated']
+    },
+    findOne:{
+      policies:['global::is-authenticated']
+    },
+    create:{
+      policies:['global::is-authenticated']
+    },
+    delete:{
+      policies:['global::is-authenticated']
+    }
+  }
+});
+

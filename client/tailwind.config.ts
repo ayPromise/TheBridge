f655e918b4f51bcee3b4bb517de1cabbec434819
ayPromise servant <fatherpother@gmail.com>
2025-05-08 00:55:09 +0300

    <frontend>: feat: initialized the default react-router v7 project (ts, tailwind)

---
+++ client/tailwind.config.ts
@@ -0,0 +1,10 @@
+export default {
+    content:['./app/**/*.{tsx, ts, css}'],
+    theme:{
+        extend:{
+            fontFamily:{
+                header:['Roboto']
+            }
+        }
+    }
+}

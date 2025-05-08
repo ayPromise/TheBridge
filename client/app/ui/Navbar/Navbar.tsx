7c71999aed7fbe1bf9d6a47be08ec52993a35a1e
ayPromise servant <fatherpother@gmail.com>
2025-05-08 12:11:04 +0300

    <frontend>: feat: improved the navbar including routing

--- client/app/ui/Navbar/Navbar.tsx
+++ client/app/ui/Navbar/Navbar.tsx
@@ -1,20 +1,20 @@
-import { useLocation } from "react-router"
+import { Link, useLocation } from "react-router"
 import classes from "./Navbar.module.css"
 
 const Navbar = () => {
     const { pathname } = useLocation()
     const links = [
         {
-            title: 'Sign up',
-            href: '/sign-up'
+            title: 'Books',
+            href: '/books'
         },
         {
             title: 'Sign in',
             href: '/sign-in'
         },
         {
-            title: 'Books',
-            href: '/books'
+            title: 'Sign up',
+            href: '/sign-up'
         }
     ]
 
@@ -23,8 +23,8 @@ const Navbar = () => {
         <nav
             className={classes.navbar}>
             <ul className={classes.navigationList}>
-                {links.map(((link, index) => (<li key={index}>
-                    <a href={link.href} className={link.href === pathname ? classes.active : ''}> {link.title}</a>
+                {links.map(((link, index) => (<li key={index} className={classes.navigationItem}>
+                    <Link to={link.href} className={link.href === pathname ? classes.active : ''}> {link.title}</Link>
                 </li>)))}
             </ul>
         </nav>

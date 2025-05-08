8a4d353077aaaad60edd3fabba3a669b477dc178
ayPromise servant <fatherpother@gmail.com>
2025-05-09 02:03:04 +0300

    <frontend>: feat: updated OAuthCallback to rely on session storage instead of localStorage

--- client/app/routes/connect.$provider.redirect.tsx
+++ client/app/routes/connect.$provider.redirect.tsx
@@ -1,44 +1,54 @@
-import { useEffect, useState } from 'react';
-import { useNavigate, useSearchParams, useParams } from 'react-router';
+import { CircularProgress } from "@mui/material";
+import { type LoaderFunctionArgs, redirect, useLoaderData } from "react-router";
+import { setUserSession } from "~/session";
+
+interface CustomError {
+    error: string,
+    status: number
+}
+
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+    const { provider } = params;
+    const url = new URL(request.url);
+    const accessToken = url.searchParams.get("access_token");
+
+    if (!accessToken || !provider) {
+        return { error: "Missing access token or provider", status: 400 };
+    }
+
+    const backendUrl = import.meta.env.VITE_STRAPI_BACKEND_URL;
+    const path = `/api/auth/${provider}/callback`;
+    const newUrl = new URL(backendUrl + path)
+    newUrl.searchParams.append("access_token", accessToken)
+
+    try {
+        const res = await fetch(newUrl);
+        const data = await res.json();
+
+        if (data.error) {
+            return { error: data.error.message || data.error, status: res.status };
+        }
+
+        if (!data.jwt || !data.user) {
+            return { error: "Invalid response from authentication server", status: 500 };
+        }
+        const headers = await setUserSession(request, data.jwt, data.user);
+        return redirect("/", { headers })
+    } catch (error: any) {
+        return { error: "An error occurred while processing the callback", status: 500 };
+    }
+};
 
 export default function OAuthCallback() {
-    const navigate = useNavigate();
-    const [params] = useSearchParams();
-    const { provider } = useParams();
-    const [text, setText] = useState('Authenticating...');
+    const { error, status } = useLoaderData<CustomError>();
 
-    useEffect(() => {
-        const accessToken = params.get('access_token')
-
-        if (!accessToken || !provider) {
-            console.error('Missing token or provider:', params.toString(), provider);
-            setText('An error occurred, please see the developer console.');
-            return;
-        }
-
-        const backendUrl = import.meta.env.VITE_STRAPI_BACKEND_URL
-        const path = `/api/auth/${provider}/callback`
-
-        const newUrl = new URL(backendUrl + path)
-        newUrl.searchParams.append("access_token", accessToken)
-
-        const fetchJWT = async () => {
-            try {
-                const res = await fetch(newUrl)
-                const data = await res.json()
-                if (data) {
-                    localStorage.setItem("jwt", data.jwt)
-                    localStorage.setItem("user", JSON.stringify(data.user))
-                }
-            } catch (error) {
-                console.log(error)
-            }
-        }
-
-        fetchJWT()
-        navigate({ pathname: "/" })
-
-    }, [navigate, params, provider]);
-
-    return <div className='w-full h-full flex justify-center items-center'>{text}</div>;
+    return (
+        <div className="w-full h-full flex justify-center items-center text-4xl">
+            {error ? (
+                <div>{error}</div>
+            ) : (
+                <CircularProgress />
+            )}
+        </div>
+    );
 }

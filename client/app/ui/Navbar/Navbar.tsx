838ce0d528f106d784b2dd6f4b8a8d00a6f9de3e
ayPromise servant <fatherpother@gmail.com>
2025-05-08 15:06:47 +0300

    <frontend>: feat: used the SplittedButton component in the Navbar

--- client/app/ui/Navbar/Navbar.tsx
+++ client/app/ui/Navbar/Navbar.tsx
@@ -1,32 +1,37 @@
 import { Link, useLocation } from "react-router"
 import classes from "./Navbar.module.css"
+import SplitButton from "./SplittedButton"
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
     const { pathname } = useLocation()
-    const links = [
+    const navigationLinks = [
         {
             title: 'Books',
             href: '/books'
-        },
-        {
-            title: 'Sign in',
-            href: '/sign-in'
-        },
-        {
-            title: 'Sign up',
-            href: '/sign-up'
         }
     ]
-
 
     return (
         <nav
             className={classes.navbar}>
-            <ul className={classes.navigationList}>
-                {links.map(((link, index) => (<li key={index} className={classes.navigationItem}>
-                    <Link to={link.href} className={link.href === pathname ? classes.active : ''}> {link.title}</Link>
-                </li>)))}
-            </ul>
+            <div className={classes.navigationContainer}>
+
+
+                <ul className={classes.navigationList}>
+                    {navigationLinks.map(((link, index) => (<li key={index} className={classes.navigationItem}>
+                        <Link to={link.href} className={link.href === pathname ? classes.active : ''}> {link.title}</Link>
+                    </li>)))}
+                </ul>
+
+
+                <SplitButton pathName={pathname} />
+
+
+            </div>
+
+            <div className={classes.copyright}>
+                Copyright ©ayPromise
+            </div>
         </nav>
     )
 }

9e19a62bcddae5fdcdd29c895aaf52bff99cc4a7
ayPromise servant <fatherpother@gmail.com>
2025-05-09 01:44:29 +0300

    <backend>: feat used pathes from .env in config

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+export default () => ({
+    'users-permissions': {
+    config: {
+      providers: {
+        github: {
+          redirectUri:  `${process.env.CLIENT_URL}/auth/github/callback`
+        },
+        google:{
+          redirectUri:  `${process.env.CLIENT_URL}/auth/google/callback`
+        }
+      },
+      jwt: {
+        expiresIn: '7d',
+      },
+    },
+  },
+});

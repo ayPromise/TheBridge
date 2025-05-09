2b20442d071c5e68c7d4638eea3bcaceb2fcc8fe
ayPromise servant <fatherpother@gmail.com>
2025-05-09 03:08:00 +0300

    <frontend>: feat: updated Navbar component so it also grabs books as a props

--- client/app/ui/Navbar/Navbar.tsx
+++ client/app/ui/Navbar/Navbar.tsx
@@ -2,12 +2,14 @@ import { Link, useLocation } from "react-router"
 import classes from "./Navbar.module.css"
 import { Button } from "@mui/material"
 import type UserSession from "types/UserSession"
+import type { Book } from "types/Book"
 
 interface NavbarProps {
     user: UserSession | null
+    books: Book[] | null
 }
 
-const Navbar: React.FC<NavbarProps> = ({ user }) => {
+const Navbar: React.FC<NavbarProps> = ({ user, books }) => {
     const { pathname } = useLocation()
 
     const onlyPublicLinks = [
@@ -31,6 +33,15 @@ const Navbar: React.FC<NavbarProps> = ({ user }) => {
                     {!user && onlyPublicLinks.map(((link, index) => (<li key={index} className={classes.navigationItem}>
                         <Link to={link.href} className={link.href === pathname ? classes.active : ''}> {link.title}</Link>
                     </li>)))}
+                </ul>
+
+
+                <ul className="flex flex-col gap-[20px]">
+                    {books && books.map((book, index) => {
+                        const pathnameID = Number(pathname.split("/").pop()) ?? -1
+                        return (<li key={index}><Link to={`book/${book.id}`} className={pathnameID === book.id ? classes.active : ''}>{book.title}</Link></li>)
+                    })
+                    }
                 </ul>
             </div>
 

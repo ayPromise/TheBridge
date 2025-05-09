e5dc574206a42ed857fceba06254a67101c68ddb
ayPromise servant <fatherpother@gmail.com>
2025-05-09 04:02:37 +0300

    <frontend>: feat: created ReduxProvider

---
+++ client/store/ReduxProvider.tsx
@@ -0,0 +1,14 @@
+import type { PropsWithChildren } from "react"
+import type React from "react"
+import { Provider } from "react-redux"
+import { store } from "store"
+
+const ReduxProvider: React.FC<PropsWithChildren> = ({ children }) => {
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    )
+}
+
+export default ReduxProvider

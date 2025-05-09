e791f82030007fb75a66a27e42d8b06b7c0a5d5d
ayPromise servant <fatherpother@gmail.com>
2025-05-09 03:08:34 +0300

    <frontend>: feat: scaffold the _auth.book.$id page

--- client/app/routes/_auth.book.$id/route.tsx
+++ client/app/routes/_auth.book.$id/route.tsx
@@ -1,6 +1,39 @@
+import { redirect, useLoaderData, type LoaderFunctionArgs } from "react-router"
+import { getJwt } from "~/session/auth"
+import type { Book } from "types/Book"
+
+const backendURL = import.meta.env.VITE_STRAPI_BACKEND_URL
+
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+    const jwt = await getJwt(request)
+
+    const apiPath = `/api/books/${params.id}`
+    const newURL = new URL(backendURL + apiPath)
+
+    try {
+        const response = await fetch(newURL, {
+            headers: {
+                "Authorization": `Bearer ${jwt}`,
+                "Content-Type": "application/json"
+            }
+        })
+
+        const data = await response.json()
+        if (!data.data)
+            return redirect("/")
+
+        return { book: data.data as Book }
+    } catch {
+        return redirect("/")
+    }
+}
+
+
 const Book = () => {
+    const { book } = useLoaderData<any>()
+
     return (
-        <div>Book</div>
+        <div>{book.title}</div>
     )
 }
 

a6ba7cbfe6c680cfd07fd01e993406672204be84
ayPromise servant <fatherpother@gmail.com>
2025-05-11 15:07:21 +0300

    <frontend>: feat: added page loader to fetch chapters

---
+++ client/app/routes/_auth.api.books.$bookId.chapters.$id/route.ts
@@ -0,0 +1,26 @@
+import { redirect, type LoaderFunctionArgs } from "react-router";
+
+// session
+import { getJwt } from "~/session/auth";
+
+// utils
+import fetchDataJWT from "utils/fetchDataJWT";
+
+// .env
+const SERVER_URL = import.meta.env.VITE_STRAPI_BACKEND_URL;
+
+export const loader = async ({ request, params }: Omit<LoaderFunctionArgs, 'context'>) => {
+  const jwt = await getJwt(request); // get jwt
+
+  try{
+    const apiPath = `/api/books/${params.bookId}/chapters/${params.id}` // the path to api endpoint
+    const newURL = new URL(SERVER_URL+apiPath) // build URL object
+    const chapter = await fetchDataJWT(newURL, jwt) // get chapter of book by id from database
+    
+    if(!chapter) redirect("/")
+
+    return chapter
+  }catch(error){
+    return redirect("/")
+  }
+};

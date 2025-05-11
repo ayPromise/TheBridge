dca8a264cf4be6586dc45ea47194ba440000d50b
ayPromise servant <fatherpother@gmail.com>
2025-05-11 16:06:50 +0300

    <frontend>: feat: installed @mui/icons-material and used in in OAuthButtons

--- client/components/OAuthButtons.tsx
+++ client/components/OAuthButtons.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@mui/material'
 import React from 'react'
-import { FaGithub, FaGoogle } from 'react-icons/fa'
+import GoogleIcon from '@mui/icons-material/Google';
+import GithubIcon from '@mui/icons-material/GitHub';
 
 const backendUrl = import.meta.env.VITE_STRAPI_BACKEND_URL;
 
@@ -23,7 +24,7 @@ const OAuthButtons: React.FC<OAuthButtonsProps> = ({ loadingRequest, setLoadingR
 
     return (
         <div className="w-[75%] px-6 py-6 bg-main-extraLight rounded-3xl mt-4">
-            <Button startIcon={<FaGoogle />} onClick={() => handleLogin('google')} variant="contained"
+            <Button startIcon={<GoogleIcon />} onClick={() => handleLogin('google')} variant="contained"
                 fullWidth
                 sx={{
                     mb: 1,
@@ -34,7 +35,7 @@ const OAuthButtons: React.FC<OAuthButtonsProps> = ({ loadingRequest, setLoadingR
             </Button>
 
             <Button
-                startIcon={<FaGithub />}
+                startIcon={<GithubIcon />}
                 onClick={() => handleLogin('github')}
                 variant="contained"
                 sx={{ backgroundColor: 'black', '&:hover': { backgroundColor: '#333' } }}

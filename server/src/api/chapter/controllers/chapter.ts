31839cf9c439011604dafb97daf5d63c32277b16
ayPromise servant <fatherpother@gmail.com>
2025-05-11 14:54:30 +0300

    <backend>: feat: overwritten find controller function for chapters and added findChapter one

--- server/src/api/chapter/controllers/chapter.ts
+++ server/src/api/chapter/controllers/chapter.ts
@@ -3,6 +3,7 @@
  */
 
 import { factories } from '@strapi/strapi'
+import book from '../../book/controllers/book';
 
 export default factories.createCoreController('api::chapter.chapter', ({strapi})=>({
     async findOne(ctx){
@@ -20,6 +21,42 @@ export default factories.createCoreController('api::chapter.chapter', ({strapi})
             return ctx.unauthorized('You have no access to that book')
 
         return ctx.send({ data: chapter });
+    },
+
+    async find(ctx){
+        const user = ctx.state.user
+        const bookId = ctx.params.id
+        const chapters = await strapi.db.query('api::chapter.chapter').findMany({where:{book:{id:bookId}},  
+        
+            populate: { book: { populate: ['owner'] } }})
+
+        if (!chapters) {
+            return ctx.notFound('No chapters were found');
+        }
+
+        if(chapters[0].book.owner.id !== user.id)
+            return ctx.unauthorized('You have no access to that book')
+
+        return ctx.send({ data: chapters });
+    },
+
+
+    async findChapter(ctx){
+        const user = ctx.state.user;
+        const bookId = ctx.params.bookId
+        const chapterId = ctx.params.chapterId
+        const chapter = await strapi.db.query('api::chapter.chapter').findOne({where:{book:{id:bookId}, id:chapterId}, populate:{
+            book:{ populate:['owner']},
+            epigraph:true
+        }})
+
+        if(!chapter)
+            return ctx.notFound("No chapter found")
+
+        if(chapter.book.owner.id !== user.id)
+            return ctx.unauthorized("You have no access to that book")
+
+        return ctx.send({data:chapter})
     }
 }));
 

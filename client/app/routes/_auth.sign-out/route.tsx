3f8e0a9cf5655f447ed59d7ec309ef97cd25e324
ayPromise servant <fatherpother@gmail.com>
2025-05-11 15:41:24 +0300

    <frontend>: feat: clear redux store when we sign-out

--- client/app/routes/_auth.sign-out/route.tsx
+++ client/app/routes/_auth.sign-out/route.tsx
@@ -1,7 +1,12 @@
+import { clearBooks } from "features/books/booksSlice";
+import { clearChapters } from "features/chapters/chapterSlice";
 import { type LoaderFunctionArgs, redirect } from "react-router";
+import { store } from "store";
 import { clearUserSession } from "~/session/auth";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
     const headers = await clearUserSession(request);
+    store.dispatch(clearBooks())
+    store.dispatch(clearChapters())
     return redirect("/sign-in", { headers });
 };

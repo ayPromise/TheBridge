77e252cd4077b23964810bb694316ec4233a39e7
ayPromise servant <fatherpother@gmail.com>
2025-05-12 13:34:28 +0300

    <frontend>: feat: added interface of how data stores after fast-xml-parser

---
+++ client/types/FB2File.ts
@@ -0,0 +1,41 @@
+export interface FB2_INote {
+    title:{p:number};
+    p: string;
+}
+
+export interface FB2_IDescription {
+    "title-info":FB2_ITitleInfo
+}
+
+export interface FB2_IAuthor{
+    "first-name":string,
+    "middle-name"?:string,
+    "last-name":string
+}
+
+
+export interface FB2_ITitleInfo{
+    author:FB2_IAuthor;
+    "book-title":string;
+}
+
+
+export interface FB2_IChapter {
+    title: {p:string};
+    p: string[];
+    epigraph?:{
+        "text-author":string;
+        p:string[];
+    }
+}
+  
+export interface FB2_IBody {
+    title:string;
+    section: FB2_IChapter[] | FB2_INote[];
+}
+
+export interface FB2_IBook{
+    body: FB2_IBody[],
+    description:FB2_IDescription
+}
+  

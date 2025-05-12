b6910b837654b0d66b24f7218f53afadc9901553
ayPromise servant <fatherpother@gmail.com>
2025-05-12 13:35:28 +0300

    <frontend>: feat: added API page to send chapters to strapi server

---
+++ client/app/routes/_auth.api.chapters.create/route.tsx
@@ -0,0 +1,23 @@
+import fetchDataJWT from "utils/fetchDataJWT";
+import { getJwt } from "~/session/auth";
+
+const SERVER_URL = import.meta.env.VITE_STRAPI_BACKEND_URL
+
+export const action = async ({ request }: { request: Request }) => {
+    const body = await request.json();
+    const jwt = await getJwt(request)
+
+    const { entityType, ...restBody } = body;
+
+    try {
+        const createdChapter = await fetchDataJWT(
+            new URL(`${SERVER_URL}/api/chapters`),
+            jwt,
+            "POST",
+            restBody
+        );
+        return { id: createdChapter.id, entityType, ...createdChapter };
+    } catch (error) {
+        return { error: 'Failed to create chapter', status: 500 };
+    }
+};

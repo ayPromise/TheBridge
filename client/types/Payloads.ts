3c4ba8c047aba8ef70b7adb21ad34ca72d649e1a
ayPromise servant <fatherpother@gmail.com>
2025-05-16 03:15:34 +0300

    <frontend>: feat: added PayloadTypes

---
+++ client/types/Payloads.ts
@@ -0,0 +1,35 @@
+import type { IEpigraph } from "./Chapter";
+import type { FB2_INote } from "./FB2File";
+
+export interface BookRelation{
+    connect: {documentId:number, status:string}[]
+}
+
+// BOOK
+export interface IBookPayload {
+    title: string;
+    author: {
+        firstName: string,
+        middleName?: string,
+        lastName:string
+    };
+}
+
+// NOTES
+export interface INotesPayload{
+    book:BookRelation,
+    notes:FB2_INote[]
+}
+
+// CHAPTERS
+export interface IChaptersPayload{
+    title: string,
+    paragraphs:string[],
+    book:BookRelation,
+    epigraph?: IEpigraph
+}
+
+export interface Payload<T> {
+    data: T,
+    entityType: string;
+}

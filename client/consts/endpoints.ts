ff4c9d9c9d146450b1a5cf6a755369a732bb61bc
ayPromise servant <fatherpother@gmail.com>
2025-05-17 04:31:16 +0300

    <frontend>: feat: updated endpoints

--- client/consts/endpoints.ts
+++ client/consts/endpoints.ts
@@ -4,15 +4,17 @@ export const serverAPIRoutes = {
     notes: '/api/notes',
     singIn: '/api/auth/local',
     signUp: '/api/auth/local/register',
-    chapterOfBook: (bookId:string, chapterId:string)=> `/api/books/${bookId}/chapters/${chapterId}`,
-    bookById: (bookId: string) => `/api/books/${bookId}`,
+    chapterOfBook: (bookId:number, chapterId:number)=> `/api/books/${bookId}/chapters/${chapterId}`,
+    bookById: (bookId: number) => `/api/books/${bookId}`,
     OAuthConnectStart: (provider:string) => `/api/connect/${provider}`,
-    OAuthConnectFinal: (provider:string) => `/api/auth/${provider}/callback`
+    OAuthConnectFinal: (provider:string) => `/api/auth/${provider}/callback`,
   } as const;
 
   export const clientAPIRoutes = {
     createBook: `/api/books/create`,
     createChapters: `/api/chapters/create`,
     createNotes: `/api/notes/create`,
-    deleteBook: (bookId: string)=>`/api/books/${bookId}/delete`
+    updateBook: (bookId:number)=> `/api/books/${bookId}/update`,
+    deleteBook: (bookId: number)=>`/api/books/${bookId}/delete`,
+    chapterOfBook: (bookId:number, chapterId:number)=>`/api/books/${bookId}/chapters/${chapterId}`
   }

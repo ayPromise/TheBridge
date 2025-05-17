9d5d07472b1dc0f0c67ddbfe1b3c3fcd2bf48dbe
ayPromise servant <fatherpother@gmail.com>
2025-05-17 04:35:53 +0300

    <frontend>: feat: changed rendering paragraphs of chapter

--- client/app/routes/_auth.book.$id/ParagraphContent.tsx
+++ client/app/routes/_auth.book.$id/ParagraphContent.tsx
@@ -1,16 +1,25 @@
 import React from "react";
+import type { IChapterParagraph } from "types/Chapter";
 
 
-const ParagraphContent: React.FC<{ list: string[] | string }> = ({ list }) => {
+const ParagraphContent: React.FC<{ list: IChapterParagraph[] }> = ({ list }) => {
     if (list && typeof list === "string")
         return <p className="mb-4 leading-relaxed text-justify">{list}</p>
 
-    if (list && typeof list === "object")
-        return list.map((paragraph: string | { a: any, "#text": string }, index) => {
-            let text = paragraph
-            if (paragraph["#text"])
-                text = paragraph["#text"]
-            return <p key={index} className="mb-4 leading-relaxed text-justify">{String(text)}</p>
+    if (Array.isArray(list))
+        return list.map((paragraph, index) => {
+            if (typeof paragraph === "object") {
+                if (paragraph.emphasis) {
+                    return <p key={index} className="mb-4 leading-relaxed text-center text-[25px] font-bold">{String(paragraph.emphasis)}</p>
+                }
+
+                if (paragraph["#text"]) {
+                    return <p key={index} className="mb-4 leading-relaxed text-justify">
+                        {paragraph["#text"]}
+                    </p>
+                }
+            }
+            return <p key={index} className="mb-4 leading-relaxed text-justify">{String(paragraph)}</p>
         })
 };
 

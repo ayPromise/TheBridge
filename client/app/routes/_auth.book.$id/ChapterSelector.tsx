164d983e3bbff3daad310a421e2ec52def3f5e04
ayPromise servant <fatherpother@gmail.com>
2025-05-18 03:18:24 +0300

    <frontend>: delete: redux from ChapterSelector component

--- client/app/routes/_auth.book.$id/ChapterSelector.tsx
+++ client/app/routes/_auth.book.$id/ChapterSelector.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 
-// redux
-import { useDispatch, useSelector } from 'react-redux'
-import { setChapters } from 'features/chapters/chapterSlice'
-
 // types
 import type { IBook } from 'types/Book'
 import type { IChapter } from 'types/Chapter'
-import type { AppDispatch, RootState } from 'store'
 
 interface ChapterSelectorProps {
     book: IBook,
@@ -18,23 +13,14 @@ interface ChapterSelectorProps {
 const CLIENT_URL = import.meta.env.VITE_CLIENT_URL
 
 const ChapterSelector: React.FC<ChapterSelectorProps> = ({ book, setCurrentChapter, currentChapter }) => {
-    const dispatch = useDispatch<AppDispatch>()
-    const chapters = useSelector((state: RootState) => state.chapters.value) as IChapter[]
-
     const handleClick = async (id: number) => {
-        const chapterFromRedux = chapters.find((ch) => ch.id === id)
-        if (chapterFromRedux) {
-            setCurrentChapter(chapterFromRedux)
-            return
-        }
+
         const urlAPI = new URL(CLIENT_URL + `/api/books/${book.id}/chapters/${id}`)
         const response = await fetch(urlAPI)
         const newChapter = await response.json()
 
         if (newChapter)
             setCurrentChapter(newChapter)
-
-        dispatch(setChapters([...chapters, newChapter]))
     }
 
     return (
